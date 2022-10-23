5c0265b8439fef7cc30c3dbd86d8f5d359c70d6f
Jumprocks <jumprocks1@gmail.com>
2022-10-23 16:40:28 -0700

    Test extra meta tag

--- drum-game-web/src/Root.ts
+++ drum-game-web/src/Root.ts
@@ -19,6 +19,13 @@ export default class Root extends Component {
     constructor() {
         super();
 
+        const meta = document.getElementsByTagName("meta");
+        for (const e of meta) {
+            if (e.content === "Drum Game Test") {
+                e.content = "Drum Game Test 2"
+            }
+        }
+
         this.DOMNode = document.getElementById("root")!;
         this.Add(new Router(pages));
     }

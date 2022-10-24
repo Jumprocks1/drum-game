675480a276745ef92d675870df727f180a866b63
Jumprocks <jumprocks1@gmail.com>
2022-10-23 17:00:25 -0700

    Try tags directly on index.html

--- drum-game-web/src/Root.ts
+++ drum-game-web/src/Root.ts
@@ -18,18 +18,6 @@ const pages: PageType[] = [
 export default class Root extends Component {
     constructor() {
         super();
-        const meta = [
-            ["og:title", "Drum Game Test Title"],
-            ["og:description", "Test description"],
-            ["og:image", "https://m.media-amazon.com/images/I/81m81sm7O7L._SS500_.jpg"],
-        ]
-
-        for (const e of meta) {
-            const m = document.createElement("meta");
-            m.setAttribute("property", e[0]);
-            m.setAttribute("content", e[1]);
-            document.head.appendChild(m)
-        }
 
         this.DOMNode = document.getElementById("root")!;
         this.Add(new Router(pages));
